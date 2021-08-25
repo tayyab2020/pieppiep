fccb1e05931cf91b7b353aea9a271821e51fc35c
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-08-25 16:00:52 +0500

    updates

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
             'modes' => [
                 //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
