8945e88aeac3cfca893dc5dcbe142e52330289da
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-10-07 00:43:18 +0500

    Updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -365,6 +365,7 @@ class ProductController extends Controller
 
     public function store(StoreValidationRequest3 $request)
     {
+        phpinfo();
                         var_dump($request->selected_model_feature22);
                         exit();
         $input = $request->all();
