75fd1f3c7baebac6d4772d40700ab74fb6a40e77
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-10-07 20:41:33 +0500

    Updates

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1903,6 +1903,19 @@
 
     $(document).ready(function() {
 
+        window.onbeforeunload = function (e) {
+            
+            e = e || window.event;
+            
+            // For IE and Firefox prior to version 4
+            if (e) {
+                e.returnValue = 'Sure?';
+                }
+                // For Safari
+                return 'Sure?';
+                
+        };
+
         $('body').on('click', '.create-feature-btn' ,function(){
 
             var id = $(this).data('id');
