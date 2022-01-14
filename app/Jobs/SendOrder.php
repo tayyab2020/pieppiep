a17a668bd13c6824ffce45f30200717287e1391c
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2022-01-14 15:54:01 +0500

    Updates

--- app/Jobs/SendOrder.php
+++ app/Jobs/SendOrder.php
@@ -79,9 +79,6 @@ class SendOrder implements ShouldQueue
             $supplier_data = User::where('id',$key)->first();
             $supplier_name = $supplier_data->name . ' ' . $supplier_data->family_name;
             $supplier_email = $supplier_data->email;
-
-            $request = new_quotations::where('id',$id)->first();
-            $quotation_invoice_number = $request->quotation_invoice_number;
             $order_number = new_orders::where('quotation_id',$id)->where('supplier_id',$key)->first();
             $order_number = $order_number->order_number;
             $filename = $order_number . '.pdf';
