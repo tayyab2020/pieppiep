ec0c95b29785130289a327304f5960244c38e475
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2022-01-20 01:32:21 +0500

    Updates

--- resources/views/user/quotations.blade.php
+++ resources/views/user/quotations.blade.php
@@ -290,7 +290,7 @@
 
                                                                                         @elseif($filteredData->count() == 0)
 
-                                                                                            <span class="btn btn-warning">Confirmation Pending {{$filteredData->count()}}</span>
+                                                                                            <span class="btn btn-warning">Confirmation Pending {{$key->data}}</span>
 
                                                                                         @else
 
