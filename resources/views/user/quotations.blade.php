2a59030bc1011c400cdaf545bc39296f1d5495a5
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2022-05-29 13:58:09 +0500

    Updates

--- resources/views/user/quotations.blade.php
+++ resources/views/user/quotations.blade.php
@@ -495,7 +495,7 @@
 
                                                                             @else
 
-                                                                                @if($key->delivered && !$key->retailer_delivered)
+                                                                                @if(!$key->retailer_delivered)
 
                                                                                     <li><a href="{{ url('/aanbieder/retailer-mark-delivered/'.$key->invoice_id) }}">{{__('text.Mark as delivered')}}</a></li>
 
