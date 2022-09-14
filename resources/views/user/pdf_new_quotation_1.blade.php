94657ce684ea48f5769a19066096c68175598867
Tayyab <tayyabkhurram62@gmail.com>
2022-09-14 15:32:06 +0500

    updates

--- resources/views/user/pdf_new_quotation_1.blade.php
+++ resources/views/user/pdf_new_quotation_1.blade.php
@@ -423,7 +423,7 @@
 
                                                             @endif
 
-                                                            @if(number_format((float)($request->qty[$i] ? $request->qty[$i] : 0), 2, '.', '') != 0 && $request->price_before_labor_old[$i] != 0)
+                                                            @if(str_replace(',', '.',$request->qty[$i]) != 0 && $request->price_before_labor_old[$i] != 0)
 
                                                                 <td style="font-size: 22px;width: 10% !important;text-align: center;">{{$request->negative_invoice ? '-' : null}} {{$request->qty[$i]}}</td>
 
@@ -440,7 +440,7 @@
 
                                                             @endif
 
-                                                            @if(number_format((float)($request->qty[$i] ? $request->qty[$i] : 0), 2, '.', '') != 0 && $request->price_before_labor_old[$i] != 0)
+                                                            @if(str_replace(',', '.',$request->qty[$i]) != 0 && $request->price_before_labor_old[$i] != 0)
 
                                                                 <td style="font-size: 22px;text-align: center;width: 15% !important;">{{number_format((float)($request->price_before_labor_old[$i]), 2, ',', '.')}}</td>
                                                                 <td style="font-size: 22px;text-align: center;width: 15% !important;">{{$request->negative_invoice ? '-' : null}} {{$arb}}</td>
